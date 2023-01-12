8a05e694c002321174fce31be6b2fd5c7068c112
Sumit Kumar Biswal <sumitbiswal98@gmail.com>
2023-01-12 16:23:53 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Using official python runtime base image
+FROM registry.access.redhat.com/ubi8/python-38
+
+# Install our requirements.txt
+
+# Copy our code from the current folder to /app inside the container
+ADD . /opt/app-root/src
+
+# Make port 80 available for links and/or publish
+EXPOSE 8080
+
+# Define our command to be run when launching the container
+#CMD ["gunicorn", "app:app", "-b", "0.0.0.0:8080", "--log-file", "-", "--access-logfile", "-", "--workers", "4", "--keep-alive", "0"]
+CMD ["python", "./square.py"]
---
+++ square.py
@@ -0,0 +1,23 @@
+# Python 3 code for hollow rectangle
+ 
+# Function to print hollow rectangle
+def print_rectangle(n, m) :
+     
+    for i in range(1, n+1) :
+        for j in range(1, m+1) :
+            if (i == 1 or i == n or
+                j == 1 or j == m) :
+                print("*", end="")           
+            else :
+                print(" ", end="")           
+         
+        print()
+ 
+ 
+# Driver program for above function
+rows = 6
+columns = 20
+print_rectangle(rows, columns)
+ 
+ 
+# This code is contributed by Nikita Tiwari.
